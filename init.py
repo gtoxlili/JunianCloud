56168d231ed28907e25dfae3ef822df35680db03
FAN VINGA <fanalcest@gmail.com>
2019-06-11 15:07:55 +0800

    use safe_load instead of load to avoid #CVE-2017-18342 and correct the load encode

---
+++ init.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+import pymysql
+import yaml
+with open("configuration.yaml", "r") as f:
+    dataMapsd = yaml.safe_load(f.read().encode('utf8'))
+
+if dataMapsd["configuration"]["Storage"] == "mysql":
+    try:
+        connsx = pymysql.connect(host=dataMapsd["configuration"]["mysql"]["host"], user=dataMapsd["configuration"]["mysql"]["user"], passwd=dataMapsd["configuration"]
+                                ["mysql"]["passwd"], port=dataMapsd["configuration"]["mysql"]["port"], charset='utf8', autocommit=True, cursorclass=pymysql.cursors.DictCursor)
+        print("mysql is connected")
+        cursor = connsx.cursor()
+        sql = "CREATE DATABASE IF NOT EXISTS jncloud"
+        cursor.execute(sql)
+        print("Database created")
+        sql_2 = '''CREATE TABLE IF NOT EXISTS `tuchuan` (
+        `id` INT NOT NULL AUTO_INCREMENT,
+        `md5` VARCHAR(255) NOT NULL,
+        `name` VARCHAR(255) NOT NULL,
+        `date` VARCHAR(255) NOT NULL,
+        PRIMARY KEY (`id`)
+        ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+        '''
+        cursor.execute(sql_2)
+        print("Data table created")
+        cursor.close()
+        connsx.close()
+    except:
+        print("Database connection error, please try again later (Also run ` init.py ` separately)")
+else:
+    pass
